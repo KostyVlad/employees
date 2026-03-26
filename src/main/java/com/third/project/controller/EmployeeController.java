2a137136e84ceee004bc09acbbe21ea560cc2d2b
KostyVlad <kostyrkinvo@gmail.com>
2026-03-26 09:15:10 +0200

    commit with updated Controller

--- src/main/java/com/third/project/controller/EmployeeController.java
+++ src/main/java/com/third/project/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.third.project.controller;
 
+
 import com.third.project.model.Employee;
 import com.third.project.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -14,13 +17,45 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeRepository.save(employee);
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if (employeeOptional.isPresent()) {
+            Employee employee = employeeOptional.get();
+            employee.setFullName(employeeDetails.getFullName());
+            employee.setAge(employeeDetails.getAge());
+            employee.setYearsInFirm(employeeDetails.getYearsInFirm());
+            employee.setSalary(employeeDetails.getSalary());
+            return ResponseEntity.ok(employeeRepository.save(employee));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
